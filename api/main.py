caca9083608940870443164c211688abd1272378
Albert Olanipekun <olanipekun_albert@live.com>
2022-05-05 23:36:28 +0100

    Quickening

--- api/main.py
+++ api/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form
 from crawler_for_api import Web_Crawler
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI()
+def include_router(app):
+    app.include_router(general_pages_router)
 
 
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    return(app)
+
+app = start_application()
+
+"""
 @app.get("/")
 def welcome():
     return "welcome"" ""world"
@@ -11,6 +22,7 @@ def welcome():
 @app.get("/greet/{name}")
 def greet(name:str):
     return {"welcome":name}
+"""
 
 @app.get("/current/destination={destination}/origin={origin}")
 def current(destination:str, origin:str):
